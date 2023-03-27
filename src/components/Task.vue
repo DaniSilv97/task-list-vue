7f1dd6f4d8a6a641e8159d1fb6eea3e0ff4a7850
Daniel Silva <danifilipesilv@gmail.com>
2023-03-27 12:09:20 +0100

    task and taskList css

---
+++ src/components/Task.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="list-task-container">
+    <input type="checkbox">
+    <div class="task-name-date">
+      <h4 class="task-name">Task name</h4>
+      <p class="task-date">doeDate</p>
+    </div>
+
+    <button class="button shadow delete">Delete</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+.list-task-container{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  padding-left: 5px;
+  padding-right: 5px;
+  border-radius: 10px;
+  background-color: var(--grey);
+  height: 40px;
+}
+.task-name-date{
+  flex: 1 1 0;
+  display: flex;
+  justify-content: space-between;
+  margin-left: 5px;
+  margin-right: 10px;
+  align-items: center;
+}
+.delete{
+  height: 30px;
+}
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class ="task-list shadow">
+    <div class="list-main-container shadow">
+      <div class="list-name-container">
+        <h2 class="list-name">Task List Name</h2>
+        <button class="button expand shadow">more...</button>
+      </div>
+      <div class="list-tasks-container">
+        <Task/>
+        <Task/>
+        <Task/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue"
+export default {
+  
+  components: { Task }
+}
+</script>
+
+<style>
+.task-list{
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 5px;
+  margin-top: 5px;
+  background-color: var(--darkGrey);
+  border-radius: 15px;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+.list-main-container{
+  background-color: var(--grey);
+  border-radius: 15px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-bottom: 5px;
+}
+.list-name-container{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 2px solid var(--darkestGrey);
+}
+
+.list-name{
+  padding-top: 5px;
+  margin-bottom: 5px;
+  margin-top: 7px;
+}
+.expand{
+  height: 30px;
+}
+.list-tasks-container{
+  margin-top: 10px;
+  margin-bottom: 5px;
+  padding: 10px;
+  padding-bottom: 1px;
+  background-color: var(--darkGrey);
+  border-radius: 15px;
+}
+</style>
